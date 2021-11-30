ffe1258108e260edd1983241dbe5a6f2610044a8
sebasalumno <a_sebastian.sotez.rojas@iespablopicasso.es>
2021-11-30 19:27:04 +0100

    dropdown done

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -9,9 +9,21 @@ import {FormControl} from '@angular/forms';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  
+  Provincias = ['hola','buenas','tardes']
 
-  constructor(private router: Router) { }
+  constructor(private router: Router) {
+    this.email = '';
+    this.username = '';
+    this.password='';
+    this.localidad = '';
+    this.direccion = '';
+
+  }
+  localidad:String;
+  direccion :String
+  email :String;
+  username: string;
+password: string;
 
   ngOnInit(): void {
     
